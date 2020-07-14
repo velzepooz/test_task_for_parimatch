b4f92b4f53d58a216db83d348cf516f0c7c4ec8c
Glib Rulev <velzepooz@gmail.com>
2020-07-14 23:24:14 +0300

    Fixes

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-# Template for FE projects
+# Test task for Parimatch International
 
-* ESlint
-* Stylelint
-* Linthtml
-* SCSS
-* Tests
+Layout marketing agency website
+
+## Used technologies
+* HTML
+* CSS
+* SASS/SCSS
+* JavaScript
+
+## Methodology
+* BEM
+
+## DEMO
+
+* [DEMO](https://velzepooz.github.io/test_task_for_parimatch/)
+
+## Run
+* npm i - install
+* npm run - starts project on localhost
+* npm run build - production build
 
--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -2,9 +2,26 @@ const contactSection = document.querySelector('.contact');
 const aboutSection = document.querySelector('.about');
 const elipseWithDot = document.querySelector('.contact__elipse-dot');
 const elipse = document.querySelector('.about__elipse');
+const personsImg = document.querySelector('.header__img-layer1');
 
 const elipseWithDotPosition = elipseWithDot.getBoundingClientRect();
 const elipsePosition = elipse.getBoundingClientRect();
+const personsImgPosition = personsImg.getBoundingClientRect();
+
+let oldValue = 0;
+
+const moveImg = (positon) => {
+  const newValue = window.pageYOffset;
+
+  if (oldValue - newValue < 0) {
+    personsImg.style.transform = `
+      translateY(${positon.y + 10}px)`;
+  } else if (oldValue - newValue > 0) {
+    personsImg.style.transform = `translateY(${positon.y}px)`;
+  }
+
+  oldValue = newValue;
+};
 
 const moveElipse = (e, position, element) => {
   const distX = e.clientX - position.x;
@@ -13,6 +30,8 @@ const moveElipse = (e, position, element) => {
   element.style.transform = `
     translate(${(distX) / 40}px, ${(-1 * distY) / 50}px)`;
 };
+
+window.addEventListener('scroll', () => moveImg(personsImgPosition));
 
 contactSection.addEventListener('mousemove', (e) => {
   return moveElipse(e, elipseWithDotPosition, elipseWithDot);
